86a3f46905805df11c8c38e2cf7bca8870ffafa3
Patrick Karney <patrickkarney@gmail.com>
2021-01-06 09:08:00 +0100

    Create class Product

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,34 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      console.log('new product: ', thisProduct);
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data: ', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+        
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +87,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
