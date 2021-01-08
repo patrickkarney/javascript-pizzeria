e448867cd2ed94e9eb6da5502af1be9d405720c0
Patrick Karney <patrickkarney@gmail.com>
2021-01-08 17:29:15 +0100

    Fix default option condition

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderFrom();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       console.log('new product: ', thisProduct);
     }
@@ -90,11 +91,12 @@
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
       thisProduct.imageVisible = thisProduct.element.querySelector(classNames.menuProduct.imageVisible);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
 
     initAccordion(){
       const thisProduct = this;
-      console.log('ThisProduct: ',thisProduct);
+      //console.log('ThisProduct: ',thisProduct);
 
       /* START: add event listener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function(event) {
@@ -102,14 +104,14 @@
         event.preventDefault();
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
-        console.log('aktywny produkt przed click: ', activeProduct);
+        
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if(activeProduct != null && activeProduct != thisProduct.element){
           activeProduct.classList.remove('active');
         } 
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
-        console.log('thisProduct element: ', thisProduct.element);
+        
       });
     }
 
@@ -138,7 +140,6 @@
       const thisProduct = this;
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -147,35 +148,33 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log('paramId',paramId, param);
 
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           const optionPrice = option.price;
-          console.log('images: ', thisProduct.imageWrapper);
-          
+                    
           // create const holding condition for checked box
           const checkedBoxCondition = formData[paramId] && formData[paramId].includes(optionId);
           // find image with class of .paramId-optionId
           const optionImage = thisProduct.imageWrapper.querySelector('.'+ paramId + '-' + optionId);
-          console.log('.paramId-optionId: ', paramId, optionId);
-          console.log('optionImage: ',optionImage);
+          
+          
           // check if there is param with a name of paramId in formData and if it includes optionId
 
           if(checkedBoxCondition){                                        // box checked
             //check if option.default is true
-            if(option.default == 1){
+            if(option.default){
               console.log('Price do not change unless was not checked');
               
-            } else if(option.default != 1){
+            } else if(!option.default){
               price = price + optionPrice;
               console.log('Price added: ', price);
               
             
             } else {                                                      // box unchecked
-              if(option.default == 1){
+              if(option.default){
                 price = price - optionPrice;
                 console.log('Price subtracted: ', price);
               } 
@@ -199,6 +198,24 @@
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
+
+    initAmountWidget(){
+      const thisProduct = this;
+
+      thisProduct.amountWidgetElem = new AmountWidget(thisProduct.amountWidgetElem);
+    }
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  class AmountWidget{
+    connstructor(element){
+      // eslint-disable-next-line no-unused-vars
+      const thisWidget = this;
+
+      console.log('AmountWidget: ', AmountWidget);
+      console.log('constructor elements: ', element);
+    }
+    
   }
 
   const app = {
