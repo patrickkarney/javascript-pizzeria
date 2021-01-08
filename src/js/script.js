d0a9f4905d2f64f2241032885e77ac994cb161f9
Patrick Karney <patrickkarney@gmail.com>
2021-01-08 16:36:17 +0100

    Add images options for checked boxes

--- src/js/script.js
+++ src/js/script.js
@@ -88,6 +88,8 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.imageVisible = thisProduct.element.querySelector(classNames.menuProduct.imageVisible);
     }
 
     initAccordion(){
@@ -140,7 +142,7 @@
 
       // set price to default price
       let price = thisProduct.data.price;
-
+      
       // for every category (param)...
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
@@ -152,21 +154,44 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           const optionPrice = option.price;
+          console.log('images: ', thisProduct.imageWrapper);
+          
+          // create const holding condition for checked box
+          const checkedBoxCondition = formData[paramId] && formData[paramId].includes(optionId);
+          // find image with class of .paramId-optionId
+          const optionImage = thisProduct.imageWrapper.querySelector('.'+ paramId + '-' + optionId);
+          console.log('.paramId-optionId: ', paramId, optionId);
+          console.log('optionImage: ',optionImage);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)){
+
+          if(checkedBoxCondition){                                        // box checked
             //check if option.default is true
             if(option.default == 1){
-              console.log('Price do not change unless was not checked')
+              console.log('Price do not change unless was not checked');
+              
             } else if(option.default != 1){
               price = price + optionPrice;
               console.log('Price added: ', price);
-            }
+              
             
-          } else {
-            if(option.default == 1){
-              price = price - optionPrice;
-              console.log('Price subtracted: ', price);
+            } else {                                                      // box unchecked
+              if(option.default == 1){
+                price = price - optionPrice;
+                console.log('Price subtracted: ', price);
+              } 
+              
             }
+          }
+          // if optionImage is not null
+          if(optionImage){
+            optionImage.classList.add(classNames.menuProduct.imageVisible);
+            // check if box is checked
+            if(checkedBoxCondition){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible); 
+            }
+              
           }
         }
       }
@@ -174,7 +199,6 @@
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
-
   }
 
   const app = {
