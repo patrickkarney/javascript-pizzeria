8b040c380a590e86090bf598f61c5d3ced83e3b3
Patrick Karney <patrickkarney@gmail.com>
2021-02-02 11:11:13 +0100

    Update eslintrc

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import {settings, select, classNames, templates} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+
+
 const app = {
   initMenu: function(){
     const thisApp = this;
