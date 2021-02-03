31c51fc626fb7286a8885cbc52088f2606124653
Patrick Karney <patrickkarney@gmail.com>
2021-02-03 17:53:19 +0100

    Add initWidget in Booking class (2bugs)

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {templates} from '../settings.js';
+import {templates, select} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 
 class Booking{
   constructor(element){
@@ -10,11 +11,23 @@ class Booking{
 
   render(element){
     const thisBooking = this;
-
+    /*generating HTML for booking page */
     const generateHTML = templates.bookingWidget();
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = generateHTML;
+    /*working with Widget on booking page */
+    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.dom.peopleAmount.addEventListener('update', function(){});
+
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('update', function(){});
   }
 }
 export default Booking;
